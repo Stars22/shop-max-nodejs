fbcf4fbceb19ab2ce0f8d13d743afd7a2a5f0f42
unknown <maildeal@gmail.com>
2019-07-03 16:26:01 +0300

    63: add public folder, static css stylesheets (express.static)

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const rootDir = require('./util/path');
 
 const app = express();
 
@@ -9,11 +8,12 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 app.listen(3000);

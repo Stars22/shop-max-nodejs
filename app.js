b14d45ad7abadae7c4bb9e5e32e769606e59f1dd
unknown <maildeal@gmail.com>
2019-07-03 12:54:46 +0300

    55:add: routes & bodyParser from 53

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(adminRoutes, shopRoutes);
 
 app.listen(3000);

1dc7ba639a4c58e52986323433ccebab604ac880
unknown <maildeal@gmail.com>
2019-07-08 15:24:27 +0300

    92:add: product price, desc, imageUrl

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,7 +11,8 @@ const getAddProductPage = (req, res, next) => {
 };
 
 const postAddProductPage = (req, res) => {
-    const product = new Product(req.body.title);
+    const { title, imageUrl, price, description } = req.body;
+    const product = new Product(title, imageUrl, description, price);
     product.save();
     res.redirect('/');
 };

a6a85fcca44da71e0b90f239f4fdbaea4d03093b
unknown <maildeal@gmail.com>
2019-07-08 16:40:20 +0300

    95:fix: eslint warnings

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const adminController = require('../controllers/admin')
+const adminController = require('../controllers/admin');
 
 router.get('/add-product', adminController.getAddProductPage);
 router.get('/products', adminController.getProductsPage);
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,8 @@
-const path = require('path');
 const express = require('express');
-const rootDir = require('../util/path')
-const adminData = require('./admin');
 
 const router = express.Router();
 
-const shopController = require('../controllers/shop')
+const shopController = require('../controllers/shop');
 
 router.get('/', shopController.getIndexPage);
 router.get('/products', shopController.getProductsPage);

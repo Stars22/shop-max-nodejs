4808fd7773f29ce33110d88b93086fdb6944bdeb
unknown <maildeal@gmail.com>
2019-07-10 17:15:16 +0300

    103:fix: cart model

--- models/cart.js
+++ models/cart.js
@@ -11,19 +11,24 @@ module.exports = class Cart {
     static addProduct(id, price) {
         fs.readFile(p, (err, data) => {
             let cart;
-            //if file doesnt exist or empty creates cart file with added product
+            //if file doesnt exist or empty creates cart object with added product
             if(err || data.length < 1) {
                 cart = { products: [{id, qty: 1}], totalPrice: price };
             //add or increase quantity if product exists in cart file
             } else {
                 cart = JSON.parse(data);
-                const updatedProducts = cart.products.map(product => {
-                    if(product.id === id) {
-                        return {...product, qty: product.qty + 1};
-                    }
-                    return product;
-                });
-                cart = { products: updatedProducts, totalPrice: cart.totalPrice + parseInt(price) };
+                let copyOfProducts = [...cart.products];
+                const existingProductIndex = copyOfProducts.findIndex(product => product.id === id);
+                let productCopy;
+                if(existingProductIndex > -1) {
+                    productCopy = { ...copyOfProducts[existingProductIndex] };
+                    productCopy.qty += 1;
+                    copyOfProducts[existingProductIndex] = productCopy;
+                } else {
+                    productCopy = {id, qty: 1};
+                    copyOfProducts = [...copyOfProducts, productCopy];
+                }
+                cart = { products: copyOfProducts, totalPrice: parseInt(cart.totalPrice) + parseInt(price) };
             }
             fs.writeFile(p, JSON.stringify(cart), err => console.log(err));
         });

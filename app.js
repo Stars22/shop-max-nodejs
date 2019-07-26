b9aed98a9ec7d255f805cb386bc02c8b999cf2b2
unknown <maildeal@gmail.com>
2019-07-26 22:07:12 +0300

    206:add login view, route, controller etc

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const errorController = require('./controllers/errors');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 // const { mongoConnect } = require('./util/database');
 const User = require('./models/user');
@@ -36,6 +37,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 app.use(errorController.get404Page);
 
 // mongoConnect(() => {

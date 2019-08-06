d516e8f1f0d07c2dc195a7dccc584751f540a00e
unknown <maildeal@gmail.com>
2019-08-06 18:36:38 +0300

    263:add: custom check for confirmPassword field to match password

--- routes/auth.js
+++ routes/auth.js
@@ -18,7 +18,14 @@ router.post('/signup', [
             }
             return true;
     }),
-    body('password', 'Please enter a password at least 6 characters').isLength({min: 6}).isAlphanumeric()],
+    body('password', 'Please enter a password at least 6 characters').isLength({min: 6}).isAlphanumeric(),
+    body('confirmPassword').custom((value, { req }) => {
+        if(value === req.body.password) {
+            return true;
+        }
+        throw new Error('Passwords have to match!')
+    })
+    ],
     authController.postSignup);
 router.post('/login', authController.postLogin);
 router.post('/logout', authController.postLogout);

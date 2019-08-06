d9ff550c044089edd7df4958ea1929f6d30f0b9e
unknown <maildeal@gmail.com>
2019-08-06 17:41:58 +0300

    260:add: custom error message for email validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -80,7 +80,7 @@ exports.postSignup = (req, res, next) => {
             .render('auth/signup', {
                 path: '/signup',
                 pageTitle: 'Signup',
-                errorMessage: errors.array()
+                errorMessage: errors.array()[0].msg
             });
     }
     User.findOne({email: email})

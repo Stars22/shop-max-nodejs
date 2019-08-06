ec14c9bcd7a33b37967e4858b90c9c399d8dbb2e
unknown <maildeal@gmail.com>
2019-08-06 18:20:46 +0300

    262:add: check(only body) for password length and alphanumeric and default error message for all checks in chain

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator');
+const { check, body } = require('express-validator');
 
 const router = express.Router();
 const authController = require('../controllers/auth');
@@ -8,16 +8,18 @@ router.get('/login', authController.getLoginPage);
 router.get('/reset', authController.getResetPage);
 router.get('/reset/:token', authController.getNewPassword);
 router.get('/signup', authController.getSignup);
-router.post('/signup', check('email')
-.isEmail()
-.withMessage('Please enter a vaild email')
-.custom((value, { req }) => {
-    if(value === 'test@test.com') {
-        throw new Error('This email adress is forbidden');
-    }
-    return true;
-}),
-authController.postSignup);
+router.post('/signup', [
+    check('email')
+        .isEmail()
+        .withMessage('Please enter a vaild email')
+        .custom((value, { req }) => {
+            if(value === 'test@test.com') {
+                throw new Error('This email adress is forbidden');
+            }
+            return true;
+    }),
+    body('password', 'Please enter a password at least 6 characters').isLength({min: 6}).isAlphanumeric()],
+    authController.postSignup);
 router.post('/login', authController.postLogin);
 router.post('/logout', authController.postLogout);
 router.post('/reset', authController.postReset);

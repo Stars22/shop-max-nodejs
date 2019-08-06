3d64a3fff5208c284f753178353cbb893d914c5a
unknown <maildeal@gmail.com>
2019-08-06 18:13:18 +0300

    261:add: custom validator for post signup route

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,16 @@ router.get('/login', authController.getLoginPage);
 router.get('/reset', authController.getResetPage);
 router.get('/reset/:token', authController.getNewPassword);
 router.get('/signup', authController.getSignup);
-router.post('/signup', check('email').isEmail(), authController.postSignup);
+router.post('/signup', check('email')
+.isEmail()
+.withMessage('Please enter a vaild email')
+.custom((value, { req }) => {
+    if(value === 'test@test.com') {
+        throw new Error('This email adress is forbidden');
+    }
+    return true;
+}),
+authController.postSignup);
 router.post('/login', authController.postLogin);
 router.post('/logout', authController.postLogout);
 router.post('/reset', authController.postReset);
